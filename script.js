9f3188ff0acf9d69c5e05ca86fc7e30d8670ab78
Tramio <tramiodev@outlook.fr>
2021-08-02 14:57:33 +0200

    create 16x16 grid, add hover css property

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const container = document.querySelector(".container");
 
 function addDivs() {
-    for(let i = 0; i < 16; i++) {
+    for(let i = 0; i < 16*16; i++) {
         const div = document.createElement("div");
+        div.classList.add("square");
         container.appendChild(div);
     }
 }
