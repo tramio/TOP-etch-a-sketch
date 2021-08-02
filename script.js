d0fde75b9d052198dd5455838c7d4022590c5a85
Tramio <tramiodev@outlook.fr>
2021-08-02 17:23:38 +0200

    add prompt for number of squares per side

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 const container = document.querySelector(".container");
+let squareNumber;
 
-function addDivs() {
-    for(let i = 0; i < 16*16; i++) {
+function addDivs(squareNumber) {
+    container.style.gridTemplateRows = `repeat(${squareNumber}, 1fr)`;
+    container.style.gridTemplateColumns = `repeat(${squareNumber}, 1fr)`;
+
+    for(let i = 0; i < squareNumber*squareNumber; i++) {
         const div = document.createElement("div");
         div.classList.add("square");
         container.appendChild(div);
     }
 }
 
-addDivs();
+addDivs(16);
 
 const squares = document.querySelectorAll(".square");
 
@@ -25,4 +29,6 @@ function clearGrid(event) {
     squares.forEach(square => {
         square.classList.remove("hovered");
     });
+    squareNumber = prompt("How many squares per side do you want?", "Yes");
+    addDivs(squareNumber);
 }
